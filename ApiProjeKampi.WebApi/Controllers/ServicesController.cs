e586c47869bffc7353304fadc91d4cd8e413cc4d
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2025-12-25 18:03:11 +0400

    Api Consume ile Servis Verilerinin Listelenmesi (24) is added

---
+++ ApiProjeKampi.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,80 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ServicesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult ServiceList()
+        {
+            var values = _context.Services.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetService(int id)
+        {
+            var value = _context.Services.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateService(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges();
+            return Ok("Hizmet ekleme işlemi başarılı");
+        }
+
+        //[HttpDelete]
+        //public IActionResult DeleteService(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return BadRequest("Id boş ola bilməz");
+        //    }
+
+        //    var Service = _context.Services
+        //        .FirstOrDefault(c => c.ServiceId == id);
+
+        //    if (Service == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Services.Remove(Service);
+        //    _context.SaveChanges();
+
+        //    return Ok(Service);
+        //}
+        [HttpDelete]
+        public IActionResult DeleteService(int id)
+        {
+            var value = _context.Services.Find(id);
+            _context.Services.Remove(value);
+            _context.SaveChanges();
+            return Ok("Hizmet silme işlemi başarılı");
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateService(Service service)
+        {
+            _context.Services.Update(service);
+            _context.SaveChanges();
+            return Ok("Hizmet güncelleme işlemi başarılı");
+        }
+    }
+}
--- ApiProjeKampi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ ApiProjeKampi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKampi.WebUI.DTOs.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProjeKampi.WebUI.ViewComponents
 {
     public class _ServiceDefaultComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage=await client.GetAsync("https://localhost:7230/api/Services/GetAll");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
