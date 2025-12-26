780a5ece0e12e92b3604f7c5e0ff08cae83ac6a2
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2025-12-26 14:47:36 +0400

    Kategorilerin UI Kısmında Consume İşlemi(25) is added

---
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuCategoryComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using ApiProjeKampi.WebUI.DTOs.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuCategoryComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuCategoryComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7230/api/Categories/GetAll");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
