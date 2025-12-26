cdad3651b369b5c88c6868fe334a7c4658e722fb
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2025-12-26 16:14:01 +0400

    Menünün UI'da Listelenmesi(26) is added

---
+++ ApiProjeKampi.WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using ApiProjeKampi.WebUI.DTOs.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuProductComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultMenuProductComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage =await client.GetAsync("https://localhost:7230/api/Products/GetAll");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
