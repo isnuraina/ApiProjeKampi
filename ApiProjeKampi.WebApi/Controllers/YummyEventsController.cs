b950dae4620b32df4bd27e1b2e20322b5fd0f9d8
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-01-05 13:20:23 +0400

    Events Api(28) is added

---
+++ ApiProjeKampi.WebApi/Controllers/YummyEventsController.cs
@@ -0,0 +1,81 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YummyEventsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public YummyEventsController(ApiContext context)
+        {
+            _context = context;
+        }
+        
+
+        [HttpGet("GetAll")]
+        public IActionResult YummyEventList()
+        {
+            var values = _context.YummyEvents.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateYummyEvent(YummyEvent YummyEvent)
+        {
+            _context.YummyEvents.Add(YummyEvent);
+            _context.SaveChanges();
+            return Ok("Etkinlik ekleme işlemi başarılı");
+        }
+
+        //[HttpDelete]
+        //public IActionResult DeleteYummyEvent(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return BadRequest("Id boş ola bilməz");
+        //    }
+
+        //    var YummyEvent = _context.YummyEvents
+        //        .FirstOrDefault(c => c.YummyEventId == id);
+
+        //    if (YummyEvent == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.YummyEvents.Remove(YummyEvent);
+        //    _context.SaveChanges();
+
+        //    return Ok(YummyEvent);
+        //}
+        [HttpDelete]
+        public IActionResult DeleteYummyEvent(int id)
+        {
+            var value = _context.YummyEvents.Find(id);
+            _context.YummyEvents.Remove(value);
+            _context.SaveChanges();
+            return Ok("Etkinlik silme işlemi başarılı");
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateYummyEvent(YummyEvent YummyEvent)
+        {
+            _context.YummyEvents.Update(YummyEvent);
+            _context.SaveChanges();
+            return Ok("Etkinlik güncelleme işlemi başarılı");
+        }
+    }
+}
