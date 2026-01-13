9edde81f0762fcb48928e7dc2cfab9e0653df806
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-01-13 18:02:04 +0400

    Admin Paneli Product Düzenlemeleri(42) is added

---
+++ ApiProjeKampi.WebUI/DTOs/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebUI.DTOs.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
