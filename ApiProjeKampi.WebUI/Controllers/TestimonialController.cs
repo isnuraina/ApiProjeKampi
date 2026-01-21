76de57bb5263d8f8a672a99762e63d87fc80fb6a
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-01-21 16:52:47 +0400

    Müşteri Yorumları(47) is added

---
+++ ApiProjeKampi.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,75 @@
+﻿using ApiProjeKampi.WebUI.DTOs.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> TestimonialList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7230/api/Testimonials/GetAll");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createTestimonialDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7230/api/Testimonials", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("TestimonialList");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7230/api/Testimonials?id=" + id);
+            return RedirectToAction("TestimonialList");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7230/api/Testimonials/GetById?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<GetTestimonialByIdDto>(jsonData);
+            return View(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateTestimonialDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client.PutAsync("https://localhost:7230/api/Testimonials/", stringContent);
+            return RedirectToAction("TestimonialList");
+        }
+    }
+}
---
+++ ApiProjeKampi.WebUI/DTOs/TestimonialDtos/CreateTestimonialDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjeKampi.WebUI.DTOs.TestimonialDtos
+{
+    public class CreateTestimonialDto
+    {
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
