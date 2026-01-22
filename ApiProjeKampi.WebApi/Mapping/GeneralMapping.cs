b2dccd05da9563d8cf1b34807298548be977e6df
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-01-22 13:23:44 +0400

    Admin Rezervasyon İşlemleri(51) is added

--- ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
+++ ApiProjeKampi.WebApi/Mapping/GeneralMapping.cs
@@ -4,6 +4,7 @@ using ApiProjeKampi.WebApi.Dtos.FeatureDtos;
 using ApiProjeKampi.WebApi.Dtos.MessageDtos;
 using ApiProjeKampi.WebApi.Dtos.NotificationDtos;
 using ApiProjeKampi.WebApi.Dtos.ProductDtos;
+using ApiProjeKampi.WebApi.Dtos.ReservationDtos;
 using ApiProjeKampi.WebApi.Entities;
 using AutoMapper;
 
@@ -45,6 +46,13 @@ namespace ApiProjeKampi.WebApi.Mapping
             CreateMap<About, CreateAboutDto>().ReverseMap();
             CreateMap<About, UpdateAboutDto>().ReverseMap();
             CreateMap<About, GetAboutByIdDto>().ReverseMap();
+
+
+
+            CreateMap<Reservation, ResultReservationDto>().ReverseMap();
+            CreateMap<Reservation, CreateReservationDto>().ReverseMap();
+            CreateMap<Reservation, UpdateReservationDto>().ReverseMap();
+            CreateMap<Reservation, GetReservationByIdDto>().ReverseMap();
 
 
         }
