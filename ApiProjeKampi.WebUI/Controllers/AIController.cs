0ce5f170a16aaa5eefb086551a558616501ad822
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-01-27 13:05:29 +0400

    changes

---
+++ ApiProjeKampi.WebUI/Controllers/AIController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class AIController : Controller
+    {
+        public IActionResult CreateRecipeWithOpenAI()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRecipeWithOpenAI(string prompt)
+        {
+            var apiKey = "";
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+            var requestData = new
+            {
+                model = "gpt-4.1-mini",
+                messages = new[]
+                {
+
+                    new {
+                        role="system",
+                        content="Sen bir restoran için yemek önerilerini yapan bir yapay zeka aracısın." +
+                    "Amacımız kullanıcı tarafından girilen malzemelere göre tarifi önerisinde bulunmak. "
+                    },
+                    new {
+                        role="user",
+                        content=prompt
+                    }
+                },
+                temperature = 0.5
+            };
+            var response = await client.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestData);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<OpenAIResponse>();
+                var content = result.choices[0].message.content;
+                ViewBag.recipe = content;
+            }
+            else
+            {
+                ViewBag.recipe = "Bir hata oluştu: " + response.StatusCode;
+            }
+            return View();
+        }
+        public class OpenAIResponse
+        {
+            public List<Choice> choices { get; set; }
+        }
+        public class Choice
+        {
+            public Message message { get; set; }
+        }
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+        }
+
+    }
+}
